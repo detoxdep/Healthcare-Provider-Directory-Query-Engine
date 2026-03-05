53d7db1eaeec8e55a90aee395489fab950f2d26c
Steven Weber <weber.stevenn@gmail.com>
2026-03-05 12:04:20 -0500

    Added code to search to read line by line, changed doctor csv to florida only

--- src/Search.java
+++ src/Search.java
@@ -1,5 +1,30 @@
 package src;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Search {
-    
+    public static void search(){
+        Doctor[] filteredSearch;
+
+        Path fileName = Paths.get("random_doctors_florida_zipcodes.csv");
+
+        try(BufferedReader reader = Files.newBufferedReader(fileName)){
+            String line;
+
+            while ((line = reader.readLine()) != null){
+                // tokenize to pass 
+                System.out.println(line);
+                
+        } catch(IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    }
 }
+    
+
--- test/testDistance.java
+++ test/testDistance.java
@@ -1,5 +1,5 @@
 package test;
-
+// iteration 2 work
 public class testDistance {
     
 }
